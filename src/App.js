da4ceb8d9d2913738e9f6212471544432bd3b798
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2025-07-07 12:46:01 -0600

    layout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './index.css';
 function App() {
   const [lang, setLang] = useState('en');
   return (
-    <div className="min-h-screen bg-black text-white font-sans">
+    <div>
       <ChatbotWidget lang={lang} setLang={setLang} />
     </div>
   );
